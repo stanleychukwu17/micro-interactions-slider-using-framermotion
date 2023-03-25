fecf7912d4815b53c0b87b4943687055c28ac8a3
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-03-25 06:06:02 +0100

    brought in the logo and some other assets needed for this project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,22 @@ import './app.scss';
 // import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
 
+import logo from '../../assets/logo/logo.svg'
+import { AiOutlineMenu } from "react-icons/ai";
+
+console.log(logo)
 const App = () => {
 
     return (
         <div className="AppMain">
-
+            <div className="logoCvr">
+                <div className="logoImg">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="">
+                    <AiOutlineMenu />
+                </div>
+            </div>
         </div>
     )
 }
