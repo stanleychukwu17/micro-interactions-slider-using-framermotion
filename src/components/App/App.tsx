79214fe4fef473bbe038ef7adaa2eda21dc2cab1
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-03-27 08:55:56 +0100

    animated all elements aside from the motherBoard

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,12 @@
-import './app.scss';
 import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
+import { gVariant } from './Variants';
+
+import './app.scss';
 
 import logo from '../../assets/logo/logo.svg'
 import imageBoard from '../../assets/images/board.jpg'
+
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaAngleLeft } from "react-icons/fa";
 import { HiArrowNarrowDown } from "react-icons/hi";
@@ -18,7 +21,7 @@ const App = () => {
 
     return (
         <div className="AppMain">
-            <motion.div className="logoCvr">
+            <motion.div variants={gVariant} custom={0} initial='initial' animate='animate' className="logoCvr">
                 <div className="logoImg">
                     <img src={logo} alt="" />
                 </div>
@@ -26,7 +29,7 @@ const App = () => {
                     <AiOutlineMenu />
                 </div>
             </motion.div>
-            <motion.div className="bckBuy_cvr">
+            <motion.div variants={gVariant} custom={1} initial='initial' animate='animate' className="bckBuy_cvr">
                 <div className="bckBuy_btn">
                     <span>
                         <FaAngleLeft />
@@ -37,22 +40,22 @@ const App = () => {
                     Buy now $59.99
                 </div>
             </motion.div>
-            <div className="midSecCvr">
+            <motion.div variants={gVariant} custom={2} initial='initial' animate='animate' className="midSecCvr">
                 <motion.div className="">Boards</motion.div>
                 <motion.div className=""><h2 className="">HiFive1 Rev</h2></motion.div>
                 <motion.div className="">
                     <p>SiFive HiFive1 is an Arduino-compatible development board featuring the SiFive Freedom E310 (FE310) SoC, making it the best way to prototype and develop RISC-V software. Designed for microcontrollers and edge computing, the FE310 instantiates a SiFive Essential™ E3 Series CPU Core Complex, a high performance 32-bit RV32IMAC core. </p>
                     Additional features include a 16KB L1 Instruction Cache, a 16KB Data SRAM scratchpad, hardware multiply/divide, a debug module, flexible clock generation with on-chip oscillators and PLLs, and a wide variety of peripherals including UARTs, QSPI, PWMs, and timers. Multiple power domains and a low-power standby mode ensure a wide variety of applications can benefit from the FE310. Software development is accelerated with the onboard FTDI FT2232 JTAG debugger connecting over USB to SiFive Freedom Studio, an Eclipse-based IDE. Additionally, your JTAG probe of choice can be connected and used with your favorite debugger and IDE. Freedom Studio is packaged with a prebuilt toolchain and example software projects. Alternatively, the SiFive Freedom Tools download package includes a pre-built toolchain, debugger, and additional command line tools that can be plugged into your own bespoke development environment.
                 </motion.div>
-                <motion.div className="btnsCvr">
-                    <motion.div className="btnItm">
-                        <button className='button'>Buy now $59</button>
-                    </motion.div>
-                    <motion.div className="dwnBtnCvr button">
-                        <HiArrowNarrowDown />
-                    </motion.div>
+            </motion.div>
+            <motion.div variants={gVariant} custom={3} initial='initial' animate='animate' className="btnsCvr">
+                <motion.div className="btnItm">
+                    <button className='button'>Buy now $59</button>
                 </motion.div>
-            </div>
+                <motion.div className="dwnBtnCvr button">
+                    <HiArrowNarrowDown />
+                </motion.div>
+            </motion.div>
             <div className="ImgBoard">
                 <div className="" onClick={() => imageBoardClicked()}>
                     <img src={imageBoard} alt="" />
--- src/components/App/Variants.ts
+++ src/components/App/Variants.ts
@@ -1,10 +1,28 @@
 import { Variant } from 'framer-motion'
 
 type general = {
-    initial: Variant
-    animate: Variant
+    initial?: Variant
+    animate?: Variant
 }
 
-export const gVariant = {
+export const gVariant: general = {
+    initial: (custom) => ({
+        y: '-30px',
+        opacity: 0,
+        transition: {
+            duration: .5,
+            delay: 0.3 * custom,
+            ease: 'easeOut'
+        }
+    }),
 
+    animate: (custom) => ({
+        y:0,
+        opacity: 1,
+        transition: {
+            duration: .5,
+            delay: 0.3 * custom,
+            ease: 'easeOut'
+        }
+    })
 }
